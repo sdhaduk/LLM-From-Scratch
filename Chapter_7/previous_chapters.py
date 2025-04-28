b689d2b5d4102c84df98ea9e559a7b8b329d99b3
sdhaduk <sagardhaduk1@gmail.com>
2025-04-28 17:03:38 -0500

    Finisihed chapter 7

--- Chapter_7/previous_chapters.py
+++ Chapter_7/previous_chapters.py
@@ -308,11 +308,13 @@ def train_model_simple(model, train_loader, val_loader, optimizer, device, num_e
             if global_step % eval_freq == 0:
                 train_loss, val_loss = evaluate_model(
                     model, train_loader, val_loader, device, eval_iter)
+                torch.mps.empty_cache()
                 train_losses.append(train_loss)
                 val_losses.append(val_loss)
                 track_tokens_seen.append(tokens_seen)
+            if global_step % 100 == 0:
                 print(f"Ep {epoch+1} (Step {global_step:06d}): "
-                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+                        f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
 
         # Print a sample text after each epoch
         generate_and_print_sample(
